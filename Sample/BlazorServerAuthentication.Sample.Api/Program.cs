e54171f5a40a88511ca7acd98e4ff45d209d32e7
davewheatcroft3 <davewheatcroft3@hotmail.com>
2023-07-05 19:24:18 +0100

    Downgraded to .net 6 for support

--- Sample/BlazorServerAuthentication.Sample.Api/Program.cs
+++ Sample/BlazorServerAuthentication.Sample.Api/Program.cs
@@ -53,25 +53,23 @@ var summaries = new[]
     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
 };
 
-app.MapGet("/weatherforecast", [Authorize] () =>
+app.MapGet("/weatherforecast", () =>
 {
     var forecast = Enumerable.Range(1, 5).Select(index =>
         new WeatherForecast
         (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            DateTime.Now.AddDays(index),
             Random.Shared.Next(-20, 55),
             summaries[Random.Shared.Next(summaries.Length)]
         ))
         .ToArray();
     return forecast;
 })
-.WithName("GetWeatherForecast")
-.WithOpenApi()
 .RequireAuthorization();
 
 app.Run();
 
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+internal record WeatherForecast(DateTime Date, int TemperatureC, string? Summary)
 {
     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 }
--- Sample/BlazorServerAuthentication.Sample.Web/Data/WeatherForecast.cs
+++ Sample/BlazorServerAuthentication.Sample.Web/Data/WeatherForecast.cs
@@ -2,7 +2,7 @@ namespace BlazerServerAuthentication.Sample.Web.Data
 {
     public class WeatherForecast
     {
-        public DateOnly Date { get; set; }
+        public DateTime Date { get; set; }
 
         public int TemperatureC { get; set; }
 
