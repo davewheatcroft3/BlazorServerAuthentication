2d97f70c361707c273076d4f7418473f59e9e9f2
davewheatcroft3 <davewheatcroft3@hotmail.com>
2023-07-14 10:22:16 +0100

    Added response detail to http client authenticator

--- BlazorServerAuthentication/HttpClientAuthenticator.cs
+++ BlazorServerAuthentication/HttpClientAuthenticator.cs
@@ -3,9 +3,19 @@ using BlazerServerAuthentication;
 
 namespace BlazorServerAuthentication;
 
+/// <summary>
+/// Inject this into your HttpClients/ApiClients to add bearer token (if authenticated).
+/// </summary>
 public interface IHttpClientAuthenticator
 {
-    Task PrepareHttpClientAsync(HttpClient httpClient);
+    /// <summary>
+    /// Will check authenticated and if so add the token to the authorization header as a bearer token.
+    /// If token expired, it will attempt to refresh the token before (and store with the provider) 
+    /// and use the refreshed token as the bearer token.
+    /// </summary>
+    /// <param name="httpClient">The http client used for the http call.</param>
+    /// <returns>True if the user is authorized and whether the refresh token was used successfully.</returns>
+    Task<(bool IsAuthenticated, bool WasRefreshed)> PrepareHttpClientAsync(HttpClient httpClient);
 }
 
 public class HttpClientAuthenticator : IHttpClientAuthenticator
@@ -17,11 +27,12 @@ public class HttpClientAuthenticator : IHttpClientAuthenticator
         _refreshTokenService = refreshTokenService;
     }
 
-    public async Task PrepareHttpClientAsync(HttpClient httpClient)
+    public async Task<(bool IsAuthenticated, bool WasRefreshed)> PrepareHttpClientAsync(HttpClient httpClient)
     {
+        var wasRefreshed = false;
         if (await _refreshTokenService.CheckIfRefreshNeededAsync())
         {
-            await _refreshTokenService.RefreshTokensAsync();
+            wasRefreshed = await _refreshTokenService.RefreshTokensAsync();
         }
 
         var token = await _refreshTokenService.GetBearerTokenAsync();
@@ -29,10 +40,14 @@ public class HttpClientAuthenticator : IHttpClientAuthenticator
         if (token != null)
         {
             httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+            return (true, wasRefreshed);
         }
         else
         {
             httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", "");
+
+            return (false, false);
         }
     }
 }
